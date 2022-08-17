0a540b12fa7410ef531ec36df2cdabaaf342d686
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-08-17 19:40:04 +0200

    Add files via upload

---
+++ src/frj/FRJ.java
@@ -0,0 +1,21 @@
+package frj;
+
+import arc.*;
+import arc.util.*;
+import frj.content.FRJBlocks;
+import frj.content.FRJTechTree;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class FRJ extends Mod{
+
+    public FRJ(){}
+
+    @Override
+    public void loadContent(){
+        new FRJBlocks().load();
+        new FRJTechTree().load();
+    }
+
+}
