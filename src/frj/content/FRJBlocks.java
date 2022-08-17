4e1b15c4fb8c97b3ff0f6491bb1145a4e7511c37
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-08-17 19:52:43 +0200

    Add files via upload

---
+++ src/frj/content/FRJBlocks.java
@@ -0,0 +1,51 @@
+package frj.content;
+
+import arc.Core;
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.content.Liquids;
+import mindustry.content.StatusEffects;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.MissileBulletType;
+import mindustry.entities.pattern.ShootAlternate;
+import mindustry.gen.Sounds;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.blocks.defense.turrets.TractorBeamTurret;
+import mindustry.world.meta.Env;
+
+import static mindustry.type.ItemStack.with;
+
+public class FRJBlocks {
+  public static Block
+         //turret
+         trio
+  ;
+  
+  public void load() {
+         trio = new ItemTurret("trio"){{
+           requirements(Category.turret, with(Items.copper, 70, Items.lead, 35));
+           ammo(
+                Items.coal, new BasicBulletType(3f, 12f) {{
+                            width = 8f;
+                            height =  8f;
+                            hitSize = 8f;
+                            lifetime = 50f;
+                            length = 8f;
+                            ammoMultiplier = 3f;
+            }}
+          );
+        size = 2;
+        reload = 12f;
+        range = 120f;
+        shoot = new ShootAlternate(){
+          {
+            shots = 3;
+            barrels = 3;
+            spread = 2f;
+            shotDelay = 4f;
+            }};
+    }
+}
