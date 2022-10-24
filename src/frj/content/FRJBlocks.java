5b1fff2bc89b73e27522a61907ac73142c71c672
Catana <77986574+Fresh791@users.noreply.github.com>
2022-10-24 17:33:20 +0200

    Update FRJBlocks.java

--- src/frj/content/FRJBlocks.java
+++ src/frj/content/FRJBlocks.java
@@ -1,49 +1,12 @@
-package frj.content;
+package flow.content;
 
+import flow.content.blocks.*;
+import java.lang.*;
 
-import arc.Core;
-import mindustry.content.Fx;
-import mindustry.content.Items;
-import mindustry.content.Liquids;
-import mindustry.content.StatusEffects;
-import mindustry.entities.bullet.MissileBulletType;
-import mindustry.entities.bullet.BasicBulletType;
-import mindustry.entities.pattern.ShootAlternate;
-import mindustry.gen.Sounds;
-import mindustry.type.Category;
-import mindustry.type.ItemStack;
-import mindustry.world.Block;
-import mindustry.world.blocks.defense.turrets.ItemTurret;
-import mindustry.world.blocks.defense.turrets.TractorBeamTurret;
-import mindustry.world.meta.Env;
+public class FlowBlocks {
+	public static Runnable[] list = {
+		FlowTurrets::load,
+	};
 
-import static mindustry.type.ItemStack.with;
-
-public class FRJBlocks {
-  public static Block trio;
-  
-  public static void load() {
-         
-         trio = new ItemTurret("trio"){{
-           requirements(Category.turret, with(Items.copper, 70, Items.lead, 35));
-           ammo(
-                Items.coal, new BasicBulletType(3f, 12f) {{
-                            width = 8f;
-                            height =  8f;
-                            hitSize = 8f;
-                            lifetime = 50f;
-                            length = 8f;
-                            ammoMultiplier = 3f;
-            }}
-          );
-        size = 2;
-        reload = 12f;
-        range = 120f;
-        health = 300;
-        shoot = new ShootAlternate(3f);
-           
-        limitRange();
-        
-      }};
-   }
+	public static void load() {for (Runnable r : list) r.run();}
 }
